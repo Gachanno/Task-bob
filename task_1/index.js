585e3d5c5f4426e8744674d844bb38a14aa1c436
Dmitriy Din <dmitriy1d01@gmail.com>
2023-09-15 11:17:27 +0300

    Updated description

--- task_1/index.js
+++ task_1/index.js
@@ -1,8 +1,23 @@
-import {encoded, translations} from './data.js'
-
+import {encoded, translations} from './data.js';
+const decoded = []
+const groupId = []
+    for (const obj of encoded){
+        const decodedObj = []
+        for (const key in obj){
+            if ("groupId" === key){
+                groupId.push(obj[key])
+            }
+            if ( ["groupId", "service", "formatSize", "ca"].includes(key))
+            {
+                decodedObj[key] = obj[key]
+                continue
+            }
+            
+            decodedObj[key] = translations[obj[key]]
+        }
+        decoded.push(decodedObj)
+    }
+console.log(decoded)
+console.log(groupId)
 console.log("Let's rock")
-console.log(encoded, translations)
 
-
-
-// console.log(decoded)
